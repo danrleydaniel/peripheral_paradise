d8430fca587d0b62fdcf3e265f6feeb0b7e075d4
danrleydaniel <danrleydaniel21@gmail.com>
2023-12-19 14:54:23 -0300

    criando um novo host testserver para os testes de mutação #7

--- peripheral_paradise/peripheral_paradise/settings.py
+++ peripheral_paradise/peripheral_paradise/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver']
 
 
 # Application definition
